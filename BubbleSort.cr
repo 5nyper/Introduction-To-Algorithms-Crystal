94b6e3d714d77ff105592b38d0b4434a2f18a344
a5i <Ap0ph1s@users.noreply.github.com>
2015-03-31 16:40:27 -0400

    Update BubbleSort.cr

--- BubbleSort.cr
+++ BubbleSort.cr
@@ -2,7 +2,21 @@ def bubblesort(array)
 	array.each_index { |i|
 		(array.size-1).downto(i) { |j|
 		# Least to Greatest
-			if array[j - 1] > array[j] 
+		if array[j - 1] > array[j] 
+                temp = array[j]
+                array[j] = array[j - 1]
+                array[j - 1] = temp
+			end
+		}
+	}
+	array
+end
+
+def bubblesort(array)
+	array.each_index { |i|
+		(array.size-1).downto(i) { |j|
+		# Least to Greatest
+		if array[j - 1] < array[j] 
                 temp = array[j]
                 array[j] = array[j - 1]
                 array[j - 1] = temp
