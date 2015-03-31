e840a4e22a31ec945291e7afaac369ecb59097b8
a5i <Ap0ph1s@users.noreply.github.com>
2015-03-31 16:39:29 -0400

    Create BubbleSort.cr

---
+++ BubbleSort.cr
@@ -0,0 +1,14 @@
+def bubblesort(array)
+	array.each_index { |i|
+		(array.size-1).downto(i) { |j|
+		# Least to Greatest
+			if array[j - 1] > array[j] 
+                temp = array[j]
+                array[j] = array[j - 1]
+                array[j - 1] = temp
+			end
+		}
+	}
+	array
+end
+	
