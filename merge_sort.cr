31d106c11c38701ec9e69ed2bc61c2ebc10522eb
a5i <Ap0ph1s@users.noreply.github.com>
2015-03-31 19:18:38 -0400

    Create merge_sort.cr

---
+++ merge_sort.cr
@@ -0,0 +1,21 @@
+def mergesort(list)
+  return list if list.size <= 1
+  mid = list.size / 2
+  left  = list[0, mid]
+  right = list[mid, list.size-mid]
+  merge(mergesort(left), mergesort(right))
+end
+
+def merge(left, right)
+  sorted = [] of Int32
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+  sorted.concat(left).concat(right)
+end
+
+puts mergesort([1,4,2,7,9,5,6,2])
